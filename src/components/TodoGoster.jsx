330d0e9fc9743e2b3101a0618433a889e6839900
ada <h.kaansolmaz@gmail.com>
2024-07-15 11:50:03 +0300

    br

--- src/components/TodoGoster.jsx
+++ src/components/TodoGoster.jsx
@@ -5,9 +5,9 @@ import { FaCheckCircle } from "react-icons/fa";
 const TodoGoster = ({ doing, setDoing }) => {
     console.log(doing);
 
-    const deleteTodo=()=>{
-        
-    }
+    const deleteTodo = (id) => {
+        setDoing(doing.filter((a) => a.id !== id));
+    };
 
     return (
         <div>
@@ -16,8 +16,14 @@ const TodoGoster = ({ doing, setDoing }) => {
                     <h3>
                         {x.text}
                         <div>
-                            <FaCheckCircle style={{color:"red", fontSize:"20px"}}/>
-                            <MdDeleteForever onClick={()=>deleteTodo(x.id)} style={{color:"red", fontSize:"25px"}} />
+                            <FaCheckCircle
+                                onClick={()=>setDoing(doing.map((a)=>a.id===x.id ? {...a, isDone:!a.isDone} : a))}
+                                style={{ color: "green", fontSize: "20px" }}
+                            />
+                            <MdDeleteForever
+                                onClick={() => deleteTodo(x.id)}
+                                style={{ color: "red", fontSize: "25px" }}
+                            />
                             {/* ikonlar */}
                         </div>
                     </h3>
