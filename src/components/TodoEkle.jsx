6a92bdd789771f25aa6c5142d68855ba69c72e20
ada <h.kaansolmaz@gmail.com>
2024-07-15 12:18:12 +0300

    btn-frm

--- src/components/TodoEkle.jsx
+++ src/components/TodoEkle.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoEkle = ({ doing, setDoing }) => {
+    const [button, setButton] = useState(true);
+
     return (
         <div>
             <header>
                 <h1>TO DO APP</h1>
-                <button className="btn" style={{ backgroundColor: "green" }}>
-                    CLOSE ADD TASK BAR
+                <button
+                    onClick={() => setButton(!button)}
+                    className="btn"
+                    style={{ backgroundColor: button ? "green" : "purple" }}
+                >
+                    {button ? "CLOSE" : "SHOW"} ADD TASK BAR
                 </button>
             </header>
 
-            <form>
-                <div className="formControl">
-                    <label htmlFor="text">Task</label>
-                    <input type="text" id="text" />
-                </div>
-                <div className="formControl">
-                    <label htmlFor="day">Day&Time</label>
-                    <input type="datetime-local" id="day" />
-                </div>
-                <button className="btn btn-submit" >SUBMIT</button>
-            </form>
+            {button && (
+                <form>
+                    <div className="formControl">
+                        <label htmlFor="text">Task</label>
+                        <input type="text" id="text" />
+                    </div>
+                    <div className="formControl">
+                        <label htmlFor="day">Day&Time</label>
+                        <input type="datetime-local" id="day" />
+                    </div>
+                    <button className="btn btn-submit">SUBMIT</button>
+                </form>
+            )}
         </div>
     );
 };
